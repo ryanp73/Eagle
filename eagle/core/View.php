aac2c30650d85c760010cf68c485a98671765efc
Ryan Pope <poperyan73@gmail.com>
2015-12-18 17:53:13 -0600

    Update View.php

--- eagle/core/View.php
+++ eagle/core/View.php
@@ -5,7 +5,6 @@ class View
 	public static function make($template, $data)
 	{
 		Autoloader::loadView('partials/Header');
-		echo $data->nickname;
 		Autoloader::loadView($template);
 		Autoloader::loadView('partials/Footer');
 	}
